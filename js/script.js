65c1af5f8fbcfe777943f33eb743d144b8a63a9f
DamianoLandi <damianolandi01@gmail.com>
2021-07-01 19:16:41 +0200

    Calcolo del Prezzo

--- js/script.js
+++ js/script.js
@@ -8,3 +8,19 @@ var age = parseInt(prompt("Quanti anni hai?"));
 
 console.log(dist);
 console.log(age);
+
+//Calcolo del prezzo
+
+if (age < 19){
+    var price = (dist * 0.21 / 100 * 80)
+}
+
+else if (age > 65){
+    var price = (dist * 0.21 / 100 * 60)
+}
+
+else {
+    var price = (dist * 0.21)
+}
+
+console.log(price);
