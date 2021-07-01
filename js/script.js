ba21d49da8eba973a3f0101126c81a47d10d4072
DamianoLandi <damianolandi01@gmail.com>
2021-07-01 19:00:08 +0200

    Created & Linked Files, Collected Variables

---
+++ js/script.js
@@ -0,0 +1,10 @@
+console.log("ok");
+
+//Raccolta dati
+
+var dist = parseInt(prompt("Quanti km vuoi viaggiare?"));
+
+var age = parseInt(prompt("Quanti anni hai?"));
+
+console.log(dist);
+console.log(age);
